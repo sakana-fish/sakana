5e6c81f691211ba32ed84c1f33b5131c0a237536
sakana <64359744+sakana-fish@users.noreply.github.com>
2020-07-06 02:29:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -26,11 +26,11 @@ async def on_ready():
     print('------')  
     await client.change_presence(activity=discord.Game(name='おさかな天国'))
 
-    
+sheetkey = os.environ['SHEETKEY']
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('/app/spreadsheet-test-282320-a54e87c8c929.json', scope)
 gc = gspread.authorize(credentials)
-wb = gc.open_by_key(os.getenv('Sheetkey'))
+wb = gc.open_by_key(sheetkey)
 ws = wb.worksheet("さかなテスト")
 ws2 = wb.worksheet("交流戦記録")
 
